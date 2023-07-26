e32b72b31ec89c441b9a4a472e67898e5948a1be
SamaNasrallah <93013128+SamaNasrallah@users.noreply.github.com>
2023-07-26 12:58:59 +0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-tagsinput@0.8.0/dist/bootstrap-tagsinput.css">
-<link rel="stylesheet"  href="{{asset('css/style.css')}}">
-<script src="{{ asset('js/script.js') }}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+
+    <link rel="stylesheet"  href="{{asset('css/style.css')}}">
+    <script src="{{ asset('js/script.js') }}"></script>
 
 <title>task 2</title>
     @include('includes.pagStyle')
 </head>
 <body>
+   
     <h1 class="nav nav-tabs" >
-        Archive System
+        <a href="{{url('/')}}" >   Archive System </a> 
     </h1>
 
 
